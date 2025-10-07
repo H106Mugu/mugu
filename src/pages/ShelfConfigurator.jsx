eb48b6bc643d9af0747d791ed4ed754acbc5e096
Yash Kotadia <yashk@hexacoder.com>
2025-10-07 11:23:15 +0530

    changed dvh to svh and added max h

--- src/pages/ShelfConfigurator.jsx
+++ src/pages/ShelfConfigurator.jsx
@@ -305,7 +305,7 @@ const ShelfConfigurator = observer(() => {
           message: loadingStore.getLoader.message,
         }}
       />
-      <div className="flex flex-col md:flex-row h-[100dvh] relative">
+      <div className="flex flex-col md:flex-row h-[100svh] max-h-[100svh] relative">
         <div className="relative bg-gray-100 w-full md:w-[66%] flex-1 md:h-full flex justify-center items-center">
           <div className="absolute top-3 left-2 flex items-center z-30">
             <Button
